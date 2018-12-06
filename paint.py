74bccdfbcc85bf016dde285666e99be58cd79fb9
Harrison <38505228+katznboyz1@users.noreply.github.com>
2018-12-05 19:15:07 -0500

    Update paint.py

--- paint.py
+++ paint.py
@@ -7,13 +7,17 @@ import _thread as thread
 import os as os
 
 def event(eventType, debug = True):
-    global applicationRunning
+    global applicationRunning, pencolor
     eventType = str(eventType)
     if (debug):
         print (eventType)
     if (eventType == 'exitProgram'):
         applicationRunning = False
         exit()
+    elif (eventType.split(':')[0] == 'setcolor'):
+        pencolor = str(eventType.split(':')[1])
+    elif (eventType == 'canvasClear'):
+        print ('This doesnt work yet')
 
 applicationRunning = True
 
@@ -38,8 +42,8 @@ QMenu{background-color:lightgray;border:1px solid #636361;color:black;font-famil
 window_header_bar_widgets_file = window_header_bar.addMenu('File')
 window_header_bar_widgets_file.setGeometry(0, 0, 40, 20)
 window_header_bar_widgets_file.setStyleSheet(window_header_bar_widgetStyleSheet)
-window_header_bar_widgets_file.addAction('New').triggered.connect(lambda: event('canvasClear'))
-window_header_bar_widgets_file.addAction('Open').triggered.connect(lambda: event('canvasOpenFile'))
+window_header_bar_widgets_file.addAction('New/Clear').triggered.connect(lambda: event('canvasClear'))
+window_header_bar_widgets_file.addAction('Open Image').triggered.connect(lambda: event('canvasOpenFile'))
 window_header_bar_widgets_file.addAction('Exit').triggered.connect(lambda: event('exitProgram'))
 
 window_header_bar_widgets_edit = window_header_bar.addMenu('Edit')
@@ -53,47 +57,60 @@ window_header_bar_widgets_edit_new_brush_color.addAction('Black').triggered.conn
 window_header_bar_widgets_edit_new_brush_color.addAction('Gray').triggered.connect(lambda: event('setcolor:gray'))
 window_header_bar_widgets_edit_new_brush_color.addAction('White').triggered.connect(lambda: event('setcolor:white'))
 window_header_bar_widgets_edit_new_brush_color.addAction('Blue').triggered.connect(lambda: event('setcolor:blue'))
-window_header_bar_widgets_edit_new_brush_color.addAction('Orange').triggered.connect(lambda: event('setcolor:orange'))
 window_header_bar_widgets_edit_new_brush_color.addAction('Red').triggered.connect(lambda: event('setcolor:red'))
-window_header_bar_widgets_edit_new_brush_color.addAction('Purple').triggered.connect(lambda: event('setcolor:purple'))
-window_header_bar_widgets_edit_new_brush_color.addAction('Yellow').triggered.connect(lambda: event('setcolor:yellow'))
+window_header_bar_widgets_edit_new_brush_color.addAction('Green').triggered.connect(lambda: event('setcolor:green'))
 
 pencolor = 'black'
-pen = PyQt5.QtGui.QPen(eval('PyQt5.QtCore.Qt.{}'.format(pencolor)), 2, PyQt5.QtCore.Qt.SolidLine)
 
 def paintEvent(x1, y1, x2, y2):
-    global renderView
-    hwh = window.height() // 2
-    hww = window.width() // 2
+    global renderView, pencolor
+    hwh = renderView.height() // 2
+    hww = renderView.width() // 2
     x1 -= hww
     x2 -= hww
     y1 -= hwh
     y2 -= hwh
     a = QGraphicsLineItem(PyQt5.QtCore.QLineF(x1, y1, x2, y2))
+    a.setPen(PyQt5.QtGui.QPen(eval('PyQt5.QtCore.Qt.{}'.format(pencolor))))
     renderView.scene().addItem(a)
-    print (x1, y1, x2, y2)
 
 renderView = QGraphicsView(window)
 renderView.move(0, window_header_bar.height())
 renderView.setScene(QGraphicsScene(window))
 renderView.setSceneRect(PyQt5.QtCore.QRectF(renderView.viewport().rect()))
 
-downpos = ''
+mousePos = False
+mouseXY = ''
+mouseLastXy = ''
 
-def mouseDown(c):
-    global downpos
-    if (downpos == ''):
-        downpos = c
-    paintEvent(downpos[0], downpos[1], c[0], c[1])
-    downpos = c
+def mouseDown(e):
+    global mousePos, mouseXY
+    mousePos = True
+    mouseXY = (e.x(), e.y())
 
-window.mousePressEvent = lambda e: mouseDown((e.x(), e.y()))
+def mouseUp(e):
+    global mousePos
+    mousePos = False
+
+def assignXY(x, y):
+    global mouseXY, mouseLastXy
+    mouseXY = (x, y)
+    if (mouseLastXy == ''):
+        mouseLastXy = (x, y)
+    paintEvent(mouseXY[0], mouseXY[1], mouseLastXy[0], mouseLastXy[1])
+    mouseLastXy = mouseXY
+
+renderView.mousePressEvent = lambda e: mouseDown(e)
+renderView.mouseReleaseEvent = lambda e: mouseUp(e)
+renderView.mouseMoveEvent = lambda e: assignXY(e.x(), e.y())
 
 def resizeThread():
-    global applicationRunning
+    global applicationRunning, mouseLastXy, mousePos
     while (applicationRunning):
         window_header_bar.resize(window.width(), 20)
         renderView.resize(window.width(), (window.height() - window_header_bar.height()))
+        if (mousePos == False):
+            mouseLastXy = ''
 
 thread.start_new_thread(resizeThread, ())
 
