0bb826096f54c00bcc7dc2c84db4e4e21e8fe294
Harrison <38505228+katznboyz1@users.noreply.github.com>
2018-12-07 10:29:18 -0500

    Update redditAppv2.py

--- redditAppv2.py
+++ redditAppv2.py
@@ -39,6 +39,17 @@ def event(e):
             window.showNormal()
             app.windowState = 'min'
 
+class headerMonitoring():
+    offset = None
+    def windowDrag(e):
+        headerMonitoring.offset = e.pos()
+    def windowMove(e):
+        x = e.globalX()
+        y = e.globalY()
+        x_w = headerMonitoring.offset.x()
+        y_w = headerMonitoring.offset.y()
+        window.move((x - x_w), (y - y_w))
+
 window = QWidget()
 window.setWindowTitle('Reddit App V2')
 window.setStyleSheet('QWidget{background-color:#2b2b2b;}')
@@ -46,6 +57,19 @@ window.setMinimumSize(500, 300)
 window.setWindowIcon(PyQt5.QtGui.QIcon('paint_application_icon.png'))
 window.resize((screen.size().width() / 2), (screen.size().height() / 2))
 window.setWindowFlags(PyQt5.QtCore.Qt.CustomizeWindowHint) 
+
+windowTopBorder = QWidget(window)
+windowTopBorder.resize(window.width(), 20)
+windowTopBorder.move(0, 0)
+windowTopBorder.setStyleSheet('QWidget{background-color:black;}')
+windowTopBorder.mousePressEvent = lambda e: headerMonitoring.windowDrag(e)
+windowTopBorder.mouseMoveEvent = lambda e: headerMonitoring.windowMove(e)
+
+windowTitle = QLabel(window)
+windowTitle.move(10, 0)
+windowTitle.setFixedHeight(20)
+windowTitle.setStyleSheet('QLabel{color:white;font-family:Calibri;background-color:transparent;}')
+windowTitle.setText('Reddit App V2')
 
 exitButton = QPushButton(window)
 exitButton.move((window.width() - exitButton.width()), 0)
@@ -82,6 +106,7 @@ def resizeThread():
         exitButton.move((window.width() - exitButton.width()), 0)
         toggleWindowStateButton.move((exitButton.x() - exitButton.width()), 0)
         minimizeButton.move((toggleWindowStateButton.x() - toggleWindowStateButton.width()), 0)
+        windowTopBorder.resize(window.width(), 20)
 
 thread.start_new_thread(resizeThread, ())
 
